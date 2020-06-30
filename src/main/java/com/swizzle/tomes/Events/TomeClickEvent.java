98892970c39cc9edb18e039255f735f41bb46934
swizzle1999 <key.key@hotmail.co.uk>
2020-06-30 01:19:11 +0100

    Took a break but updated it to 1.16 and finally finished the rewards pages. Now also shows % of drop chance in rewards page. It is essentially good to launch but just needs a ton of polish and features added. New quests in specific

--- src/main/java/com/swizzle/tomes/Events/TomeClickEvent.java
+++ src/main/java/com/swizzle/tomes/Events/TomeClickEvent.java
@@ -1,5 +1,6 @@
 package com.swizzle.tomes.Events;
 
+import com.swizzle.tomes.GUI.PlayerPagesContainer;
 import com.swizzle.tomes.TomeClasses.Tome;
 import com.swizzle.tomes.Tomes;
 import org.bukkit.Bukkit;
@@ -15,60 +16,191 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class TomeClickEvent implements Listener {
+
+    HashMap<UUID, PlayerPagesContainer> playerPageMap = new HashMap<>();
+
     @EventHandler
     public void onTomeGUIClick(InventoryClickEvent e){
 
-        if (e.getView().getTitle().equalsIgnoreCase("tomes") || e.getView().getTitle().equalsIgnoreCase("Tome Rewards")){
-            if (e.getCurrentItem() != null && e.getCurrentItem().getItemMeta().getLore() != null && e.getCurrentItem().getItemMeta().getLore().get(0).equals("Random Quests For Rewards!")){
-                //Check which tome was clicked
-                for (Tome tome : Tomes.getTomes()){
-                    if (e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(tome.getTomeDisplayName())){
-                        if (e.getClick() == ClickType.LEFT) {
-                            if (((Player) e.getWhoClicked()).getLevel() >= Tomes.getInstance().getConfig().getInt("tomes." + tome.getTomeVariableName() + ".cost")) {
-                                ((Player) e.getWhoClicked()).setLevel(((Player) e.getWhoClicked()).getLevel() - Tomes.getInstance().getConfig().getInt("tomes." + tome.getTomeVariableName() + ".cost"));
-                                e.getWhoClicked().getInventory().addItem(tome.giveBook());
-                            } else {
-                                e.getWhoClicked().sendMessage(ChatColor.RED + "Sorry, You do not have the required levels to purchase this tome");
-                            }
-                        } else if (e.getClick() == ClickType.RIGHT){
-                            Player player = (Player) e.getWhoClicked();
-                            Inventory rewardsInventory = Bukkit.createInventory(player, 54, "Tome Rewards");
+        String title = e.getView().getTitle();
+        List<String> titleSplitBySpace= Arrays.asList(title.split(" "));
 
-                            rewardsInventory.setItem(45, new ItemStack(Material.OAK_SIGN));
+        //Yes... i hate this as much as you do
+        boolean isATomeMenu = false;
+        for (String string : titleSplitBySpace){
+            if (string.toLowerCase().contains("tome")){
+                isATomeMenu = true;
+                break;
+            }
+        }
 
-                            ConfigurationSection tomeRewardsItemsConfig = Tomes.getInstance().getConfig().getConfigurationSection("tomes." + tome.getTomeVariableName() + ".rewards.items");
-                            ConfigurationSection tomeRewardsWeightsConfig = Tomes.getInstance().getConfig().getConfigurationSection("tomes." + tome.getTomeVariableName() + ".rewards.weights");
-
-                            if (tomeRewardsItemsConfig != null && tomeRewardsWeightsConfig != null) {
-                                for (String key : tomeRewardsItemsConfig.getKeys(false)) {
-                                    ItemStack rewardItem = tomeRewardsItemsConfig.getItemStack(key);
-                                    ItemMeta rewardItemMeta = rewardItem.getItemMeta();
-                                    ArrayList<String> rewardItemLore = new ArrayList<String>();
-
-                                    rewardItemLore.add("Weight: " + tomeRewardsWeightsConfig.getString(key));
-
-                                    rewardItemMeta.setLore(rewardItemLore);
-                                    rewardItem.setItemMeta(rewardItemMeta);
-
-                                    rewardsInventory.addItem(rewardItem);
+        if (isATomeMenu){
+            e.setCancelled(true);
+            if (e.getCurrentItem() != null){
+                if (e.getCurrentItem().getItemMeta().getLore() != null && e.getCurrentItem().getItemMeta().getLore().get(0).equals("Random Quests For Rewards!")){
+                    //Check which tome was clicked
+                    for (Tome tome : Tomes.getTomes()){
+                        if (e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(tome.getTomeDisplayName())){
+                            if (e.getClick() == ClickType.LEFT) {
+                                if (((Player) e.getWhoClicked()).getLevel() >= Tomes.getInstance().getConfig().getInt("tomes." + tome.getTomeVariableName() + ".cost")) {
+                                    ((Player) e.getWhoClicked()).setLevel(((Player) e.getWhoClicked()).getLevel() - Tomes.getInstance().getConfig().getInt("tomes." + tome.getTomeVariableName() + ".cost"));
+                                    e.getWhoClicked().getInventory().addItem(tome.giveBook());
+                                } else {
+                                    e.getWhoClicked().sendMessage(ChatColor.RED + "Sorry, You do not have the required levels to purchase this tome");
                                 }
+                            } else if (e.getClick() == ClickType.RIGHT){
+                                Player player = (Player) e.getWhoClicked();
+
+                                ConfigurationSection tomeRewardsItemsConfig = Tomes.getInstance().getConfig().getConfigurationSection("tomes." + tome.getTomeVariableName() + ".rewards.items");
+                                ConfigurationSection tomeRewardsWeightsConfig = Tomes.getInstance().getConfig().getConfigurationSection("tomes." + tome.getTomeVariableName() + ".rewards.weights");
+
+                                //Storing all of the items we need to fit into these pages in an array list
+                                ArrayList<ItemStack> allItems = new ArrayList<>();
+                                ArrayList<Integer> allItemsWeights = new ArrayList<>();
+                                int totalWeightOfAllItems = 0;
+                                if (tomeRewardsItemsConfig != null && tomeRewardsWeightsConfig != null) {
+                                    for (String key : tomeRewardsItemsConfig.getKeys(false)) {
+                                        ItemStack rewardItem = tomeRewardsItemsConfig.getItemStack(key);
+                                        ItemMeta rewardItemMeta = rewardItem.getItemMeta();
+                                        ArrayList<String> rewardItemLore = new ArrayList<String>();
+
+                                        rewardItemMeta.setLore(rewardItemLore);
+                                        rewardItem.setItemMeta(rewardItemMeta);
+
+                                        allItems.add(rewardItem);
+                                        allItemsWeights.add(Integer.parseInt(tomeRewardsWeightsConfig.getString(key)));
+                                        totalWeightOfAllItems += Integer.parseInt(tomeRewardsWeightsConfig.getString(key));
+
+                                    }
+                                }
+
+                                for (int i = 0; i < allItems.size(); i++){
+                                    ItemMeta itemMeta = allItems.get(i).getItemMeta();
+
+                                    List<String> itemLore;
+                                    if (itemMeta.getLore() == null){
+                                        itemLore = new ArrayList<>();
+                                    } else {
+                                        itemLore = itemMeta.getLore();
+                                    }
+
+
+//                                    System.out.println((float)totalWeightOfAllItems);
+//                                    System.out.println((float)allItemsWeights.get(i));
+                                    float dropChance = ((float)allItemsWeights.get(i) / (float)totalWeightOfAllItems)*100;
+
+                                    itemLore.add("Drop Chance: " + dropChance + "%");
+                                    itemLore.add("Weight: " + allItemsWeights.get(i));
+
+                                    itemMeta.setLore(itemLore);
+                                    allItems.get(i).setItemMeta(itemMeta);
+
+                                }
+                                for (ItemStack item : allItems){
+
+                                }
+
+                                //Creating the place holder inventory with only the navigation in it
+                                //This allows us to find out how many open slots we have to fill with items
+                                //It also allows us to take copies of this inventory and use it for all the other inventories to fill
+                                Inventory rewardsInventory = Bukkit.createInventory(player, 54, "Tome Rewards");
+
+                                //Left Arrow
+                                ItemStack leftArrow = new ItemStack(Material.OAK_SIGN);
+                                ItemMeta leftArrowMetaData = leftArrow.getItemMeta();
+                                leftArrowMetaData.setDisplayName("Last Page");
+                                leftArrow.setItemMeta(leftArrowMetaData);
+
+                                rewardsInventory.setItem(45, leftArrow);
+
+                                //Right Arrow
+                                ItemStack rightArrow = new ItemStack(Material.OAK_SIGN);
+                                ItemMeta rightArrowMetaData = leftArrow.getItemMeta();
+                                rightArrowMetaData.setDisplayName("Next Page");
+                                rightArrow.setItemMeta(rightArrowMetaData);
+
+                                rewardsInventory.setItem(53, rightArrow);
+
+                                //Red Glass
+                                rewardsInventory.setItem(46, new ItemStack(Material.RED_STAINED_GLASS_PANE));
+                                rewardsInventory.setItem(47, new ItemStack(Material.RED_STAINED_GLASS_PANE));
+                                rewardsInventory.setItem(48, new ItemStack(Material.RED_STAINED_GLASS_PANE));
+                                rewardsInventory.setItem(49, new ItemStack(Material.RED_STAINED_GLASS_PANE));
+                                rewardsInventory.setItem(50, new ItemStack(Material.RED_STAINED_GLASS_PANE));
+                                rewardsInventory.setItem(51, new ItemStack(Material.RED_STAINED_GLASS_PANE));
+                                rewardsInventory.setItem(52, new ItemStack(Material.RED_STAINED_GLASS_PANE));
+
+                                //Finding out how many empty slots we have available in the inventory to fill
+                                int numberOfEmptySlots = 0;
+                                for (ItemStack slot : rewardsInventory.getContents()){
+                                    if (slot == null){
+                                        numberOfEmptySlots += 1;
+                                    }
+                                }
+
+                                //Creating an array list of inventories
+                                ArrayList<Inventory> allInventories = new ArrayList<>();
+
+                                //Finding out how many inventories (pages) are actually required based on the number of items we need to fit in and the number of empty slots we have per page
+                                int numberOfRequiredInventories = (int) Math.ceil((float)allItems.size() / (float)numberOfEmptySlots);
+
+                                //Formula breaks when there is no rewards so this should be a fix
+                                if (numberOfRequiredInventories == 0){
+                                    numberOfRequiredInventories = 1;
+                                }
+                                //A counter to keep track of what item we are adding from the list
+                                int currentAllItemsIndex = 0;
+                                //Creating all the inventories and filling them with items
+                                for (int i = 0; i < numberOfRequiredInventories; i++){
+
+                                    Inventory inventory = Bukkit.createInventory(player, 54, "Tome Rewards - Page " + (i + 1));
+                                    inventory.setContents(rewardsInventory.getContents());
+
+                                    for (int j = 0; j < numberOfEmptySlots; j++){
+                                        if (currentAllItemsIndex > allItems.size() - 1){
+                                            break;
+                                        }
+
+                                        if (allItems.size() > 0) {
+                                            inventory.setItem(j, allItems.get(currentAllItemsIndex));
+                                        }
+
+                                        currentAllItemsIndex += 1;
+                                    }
+
+                                    allInventories.add(inventory);
+                                }
+
+                                //Creating a player pages container object and instantiating it with the player who it belongs to, the page number they are on and all of the inventories (pages)
+                                PlayerPagesContainer playerPagesContainer = new PlayerPagesContainer(player.getUniqueId(), 0, allInventories);
+                                playerPageMap.put(playerPagesContainer.getPlayerID(), playerPagesContainer);
+
+                                //Opening the correct inventory
+                                e.getWhoClicked().openInventory(playerPagesContainer.getCurrentInventory());
                             }
 
-                            for (int i = 0; i < 300; i++){
-                                rewardsInventory.addItem(new ItemStack(Material.DIRT, 64));
-                            }
-
-                            e.getWhoClicked().openInventory(rewardsInventory);
                         }
-
                     }
                 }
-            }
+                else if (e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("next page")){
+                    Player player = (Player) e.getWhoClicked();
 
-            e.setCancelled(true);
+                    PlayerPagesContainer playerPagesContainer = playerPageMap.get(player.getUniqueId());
+                    playerPagesContainer.incrementPage();
+
+                    e.getWhoClicked().openInventory(playerPagesContainer.getCurrentInventory());
+                } else if (e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("last page")){
+                    Player player = (Player) e.getWhoClicked();
+
+                    PlayerPagesContainer playerPagesContainer = playerPageMap.get(player.getUniqueId());
+                    playerPagesContainer.decrementPage();
+
+                    e.getWhoClicked().openInventory(playerPagesContainer.getCurrentInventory());
+                }
+            }
         }
 
 
---
+++ src/main/java/com/swizzle/tomes/GUI/PlayerPagesContainer.java
@@ -0,0 +1,60 @@
+package com.swizzle.tomes.GUI;
+
+import org.bukkit.inventory.Inventory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class PlayerPagesContainer {
+    private UUID playerID;
+    private int pageNumber;
+    ArrayList<Inventory> pages;
+
+    public PlayerPagesContainer(UUID playerID, int pageNumber, ArrayList<Inventory> pages){
+        this.playerID = playerID;
+        this.pageNumber = pageNumber;
+        this.pages = pages;
+    }
+
+    public Inventory getCurrentInventory(){
+        System.out.println(pages.size());
+        return pages.get(pageNumber);
+    }
+
+    public void incrementPage(){
+        if (pageNumber < pages.size()-1) {
+            this.pageNumber += 1;
+        }
+    }
+
+    public void decrementPage(){
+        if (pageNumber > 0) {
+            this.pageNumber -= 1;
+        }
+    }
+
+    public UUID getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(UUID playerID) {
+        this.playerID = playerID;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public ArrayList<Inventory> getPages() {
+        return pages;
+    }
+
+    public void setPages(ArrayList<Inventory> pages) {
+        this.pages = pages;
+    }
+}
